a582d61b31f655fd3934c31e465d2dae92aa0f7d
DTT2411 <david.thomson2411@gmail.com>
2025-03-24 12:11:33 +0000

    Add anticipated functions and their docstrings

--- run.py
+++ run.py
@@ -16,3 +16,36 @@ survey = SHEET.worksheet("survey_results")
 data = survey.get_all_values()
 
 print(data)
+
+def get_survey_data():
+    """
+    Gets survey input for a given responder from the 
+    user.
+    """
+
+def update_sheet(new_data_row):
+    """
+    Updates the survey spreadsheet with a list of new
+    values, provided by the user.
+    """
+
+def read_user_data(name):
+    """
+    Reads a row of data from the spreadsheet based on 
+    the employees name, which is passed by user.
+    """
+
+def validate_data(values):
+    """
+    Checks that the user input intended to be submitted 
+    to the spreadsheet is in a valid format.
+    """
+
+def main():
+    """
+    Run all program functions
+    """
+    print("Hello world")
+
+print("Welcome to DT Survey Analytics.\n")
+main()
